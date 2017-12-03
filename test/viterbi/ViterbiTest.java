baf818308294e532f9a25bca083c55ff2b062cc5
Attilio <parrotchaffinch@gmail.com>
2017-12-03 18:44:06 +0100

    Futher test cases for viterbi.

--- test/viterbi/ViterbiTest.java
+++ test/viterbi/ViterbiTest.java
@@ -20,4 +20,51 @@ public class ViterbiTest {
 		assertThat(states, is(expected));
 	}
 
+	@Test
+	public void oneStateOneObservation() {
+		double [] initialDistrib = {1.0};
+		double [][] transitionProbs = {{1.0}};
+		double [][] emissionProbs = {{1.0}};
+		int [] observations = {0};
+		
+		final int [] states = viterbi(1, 1, initialDistrib, transitionProbs, emissionProbs, observations);
+		final int [] expected = {0};
+		assertThat(states, is(expected));
+	}
+
+	@Test
+	public void oneStateTwoObservations() {
+		double [] initialDistrib = {1.0};
+		double [][] transitionProbs = {{1.0}};
+		double [][] emissionProbs = {{0.4, 0.5}};
+		int [] observations = {1, 1};
+		
+		final int [] states = viterbi(1, 2, initialDistrib, transitionProbs, emissionProbs, observations);
+		final int [] expected = {0, 0};
+		assertThat(states, is(expected));
+	}
+
+	@Test
+	public void twoStatesOneObservation() {
+		double [] initialDistrib = {0.6, 0.4};
+		double [][] transitionProbs = {{0.7, 0.3}, {0.4, 0.6}};
+		double [][] emissionProbs = {{1.0}, {1.0}};
+		int [] observations = {0, 0};
+		
+		final int [] states = viterbi(2, 1, initialDistrib, transitionProbs, emissionProbs, observations);
+		final int [] expected = {0, 0};
+		assertThat(states, is(expected));
+	}
+
+	@Test
+	public void twoStatesTwoObservations() {
+		double [] initialDistrib = {0.6, 0.4};
+		double [][] transitionProbs = {{0.7, 0.3}, {0.4, 0.6}};
+		double [][] emissionProbs = {{0.6, 0.4}, {0.6, 0.4}};
+		int [] observations = {0, 0};
+		
+		final int [] states = viterbi(2, 2, initialDistrib, transitionProbs, emissionProbs, observations);
+		final int [] expected = {0, 0};
+		assertThat(states, is(expected));
+	}
 }
