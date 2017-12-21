9d2ff747b0a05e8c1742594b51dbfc9d9dd66e97
Attilio <parrotchaffinch@gmail.com>
2017-12-21 16:56:59 +0100

    Add wrapper for viterbi.

--- src/viterbi/Viterbi.java
+++ src/viterbi/Viterbi.java
@@ -1,5 +1,11 @@
 package viterbi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Viterbi {
 
@@ -68,6 +74,13 @@ public class Viterbi {
 			}
 		}
 		
+		
+		for (int i = 0; i < numStates; ++i) {
+			for (int j = 0; j < observations.length; ++j) {
+				System.out.println("state #" + i + " observation #" + j + " --> " + stateProbsForObservations[i][j] + " " + prevStates[i][j]);
+			}
+		}
+		
 		// ------------------------------------------------------------
 		int stateWithMaxProb = 0;
 		double maxProb = stateProbsForObservations[stateWithMaxProb][observations.length - 1];
@@ -90,15 +103,171 @@ public class Viterbi {
 		return result;
 	}
 
+
+	public static class Key<T> {
+		public T key1, key2;
+
+		public Key(T key1, T key2) {
+			this.key1 = key1;
+			this.key2 = key2;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof Key))
+				return false;
+			Key<T> ref = (Key<T>) obj;
+			return this.key1.equals(ref.key1) && this.key2.equals(ref.key2);
+		}
+
+		@Override
+		public int hashCode() {
+			return key1.hashCode() ^ key2.hashCode();
+		}
+	}
+	
+
+	public static List<String> viterbiWrapper(Set<String> states,
+										Set<String> emissions,
+			Map<Key<String>, Double> transitionProbs,
+			Map<Key<String>, Double> emissionProbs,
+			Map<String, Double> initProbs,
+			List<String> observations) {
+		Map<String, Integer> stateIdxs = new HashMap<>();
+		Map<Integer, String> revStateIdxs = new HashMap<>();
+		Map<String, Integer> emissionIdxs = new HashMap<>();
+		
+		double [] initialDistrib = new double[states.size()];
+		double [][] transProbs = new double[states.size()][states.size()];
+		double [][] emitProbs = new double[states.size()][emissions.size()];
+		int [] obses = new int[observations.size()];
+		
+		for (String state: states) {
+			stateIdxs.put(state, stateIdxs.size());
+			revStateIdxs.put(stateIdxs.get(state), state);
+		}
+		for (String emission: emissions) {
+			emissionIdxs.put(emission, emissionIdxs.size());
+		}
+
+		
+		for (String state: states) {
+			int idx1 = stateIdxs.get(state);
+			initialDistrib[idx1] = initProbs.getOrDefault(state, 0.0);
+		}
+		
+		for (String state: states) {
+			for (String state2: states) {
+				double transitionProb = transitionProbs.getOrDefault(new Key<String>(state, state2), 0.0);
+				int idx1 = stateIdxs.get(state);
+				int idx2 = stateIdxs.get(state2);
+				transProbs[idx1][idx2] = transitionProb;
+			}
+		}
+		
+
+		for (String state: states) {
+			for (String emission: emissions) {
+				double emissionProb = emissionProbs.getOrDefault(new Key<String>(state, emission), 0.0);
+				int idx1 = stateIdxs.get(state);
+				int idx2 = emissionIdxs.get(emission);
+				emitProbs[idx1][idx2] = emissionProb;
+			}
+		}
+
+		for (int i = 0; i < observations.size(); ++i) {
+			obses[i] = emissionIdxs.get(observations.get(i));
+		}
+		
+		int [] result = viterbi(states.size(), emissions.size(),
+				initialDistrib, transProbs, emitProbs, obses);
+		List<String> ret = new ArrayList<String>(result.length);
+		
+		for (int r: result) {
+			ret.add(revStateIdxs.get(r));
+		}
+		
+		return ret;
+	}
+	
+	
 	public static void main(String[] args) {
+/*
 		double [] initialDistrib = {0.6, 0.4};
 		double [][] transitionProbs = {{0.7, 0.3}, {0.4, 0.6}};
 		double [][] emissionProbs = {{0.5, 0.4, 0.1}, {0.1, 0.3, 0.6}};
 		int [] observations = {0, 1, 2};
-		
+
 		int [] states = viterbi(2, 3, initialDistrib, transitionProbs, emissionProbs, observations);
+*/
+/*		
+//                                       P    V    N    D
+		double [] initialDistrib = 
+                                       {1.0, 0.0, 0.0, 0.0};
+		double [][] transitionProbs = 
+                                      {{0.0, 1.0, 0.0, 0.0},     // P
+                                       {0.0, 0.0, 0.0, 1.0},     // V
+                                       {0.0, 0.0, 0.0, 0.0},     // N
+                                       {0.0, 0.0, 1.0, 0.0},     // D
+                                      };
+		double [][] emissionProbs = {// Je0  Mange1  Tue2  Pomme3  Mouche4  Une5
+                                     {  1.0,  0.0,   0.0,   0.0,    0.0,    0.0},    // P
+                                     {  0.0,  0.5,   0.5,   0.0,    0.0,    0.0},    // V
+                                     {  0.0,  0.0,   0.0,   0.5,    0.5,    0.0},    // N
+                                     {  0.0,  0.0,   0.0,   0.0,    0.0,    1.0},    // D
+                                    };
+		int [] observations = {0, 1, 5, 3};
+
+
+		
+		int [] states = viterbi(4, 6, initialDistrib, transitionProbs, emissionProbs, observations);
+		
 		for (int state: states) {
 			System.out.println(state);
 		}
+
+*/
+
+		List<String> states = viterbiWrapper(
+		new HashSet<String>(){{
+			add("P"); add("V"); add("N"); add("D");
+		}},
+		new HashSet<String>(){{
+			add("Je"); add("Te"); add("Ma"); add("Manage");
+			add("Tue"); add("Montre"); add("Pomme"); add("Mouche");
+			add("Une");
+		}},
+		new HashMap<Key<String>, Double>() {{
+			put(new Key<String>("P", "V"), 0.3333333);
+			put(new Key<String>("P", "P"), 0.3333333);
+			put(new Key<String>("P", "N"), 0.3333334);
+			put(new Key<String>("V", "D"), 0.5);
+			put(new Key<String>("V", "P"), 0.5);
+			put(new Key<String>("D", "N"), 1.0);
+		}},
+		new HashMap<Key<String>, Double>() {{
+			put(new Key<String>("P", "Je"),     0.3333333);
+			put(new Key<String>("P", "Te"),     0.3333333);
+			put(new Key<String>("P", "Ma"),     0.3333334);
+			put(new Key<String>("V", "Mange"),  0.3333333);
+			put(new Key<String>("V", "Tue"),    0.3333333);
+			put(new Key<String>("V", "Montre"), 0.3333334);
+			put(new Key<String>("N", "Pomme"),  0.3333333);
+			put(new Key<String>("N", "Mouche"), 0.3333333);
+			put(new Key<String>("N", "Montre"), 0.3333334);
+			put(new Key<String>("D", "Une"),    1.0);
+		}}, 
+		new HashMap<String, Double>() {{
+			put("P", 1.0);
+		}},
+		new ArrayList<String>() {{
+			add("Je");
+			add("Te");
+			add("Montre");
+			add("Ma");
+			add("Montre");
+		}});
+		
+		System.out.println(states);
 	}
 }
--- test/viterbi/ViterbiTest.java
+++ test/viterbi/ViterbiTest.java
@@ -1,9 +1,19 @@
 package viterbi;
 
 import static viterbi.Viterbi.viterbi;
+import static viterbi.Viterbi.viterbiWrapper;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+import viterbi.Viterbi.Key;
 import static org.hamcrest.Matchers.*;
 
 public class ViterbiTest {
@@ -67,4 +77,56 @@ public class ViterbiTest {
 		final int [] expected = {0, 0};
 		assertThat(states, is(expected));
 	}
+	
+	@Test
+	public void wrapperWithSampleFromWikipedia() {
+		List<String> states = viterbiWrapper(set("Healthy", "Fever"),
+				set("Normal", "Cold", "Dizzy"),
+				ViterbiTest.<Key<String>, Double>map(
+						new Key<String>("Healthy", "Healthy"), 0.7,
+						new Key<String>("Healthy", "Fever"), 0.3,
+						new Key<String>("Fever", "Healthy"), 0.4,
+						new Key<String>("Fever", "Fever"), 0.6
+					),
+				ViterbiTest.<Key<String>, Double>map(
+						new Key<String>("Healthy", "Normal"), 0.5,
+						new Key<String>("Healthy", "Cold"), 0.4,
+						new Key<String>("Healthy", "Dizzy"), 0.1,
+						new Key<String>("Fever", "Normal"), 0.1,
+						new Key<String>("Fever", "Cold"), 0.3,
+						new Key<String>("Fever", "Dizzy"), 0.6
+					),
+				ViterbiTest.<String, Double>map(
+						"Healthy", 0.6,
+						"Fever", 0.4
+					),
+				list("Normal", "Cold", "Dizzy"));
+		final List<String> expected = list("Healthy", "Healthy", "Fever");
+		assertThat(states, is(expected));
+	}
+	
+	
+	private static <T> Set<T> set(T ... elements) {
+		Set<T> ret = new HashSet<>();
+		for (T e: elements) {
+			ret.add(e);
+		}
+		return ret;
+	}
+	
+	private static <K, V> Map<K, V> map(Object ... keysAndValues) {
+		if (keysAndValues.length % 2 != 0) {
+			throw new IllegalArgumentException("keysAndValues.length = " + keysAndValues.length);
+		}
+		Map<K, V> result = new HashMap<K, V>();
+		for (int i = 0; i < keysAndValues.length - 1; i += 2) {
+			result.put((K) keysAndValues[i], (V) keysAndValues[i + 1]);
+		}
+		return result;
+	}
+	
+	private static <T> List<T> list(T ... elements) {
+		return Arrays.asList(elements);
+	}
+
 }
